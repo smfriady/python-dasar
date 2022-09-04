0f4fdfa2d124086baf8ce8270d8fc38a3f7aec90
zoenasun <zoenasun@gmail.com>
2022-09-04 23:15:39 +0700

    input user

---
+++ input_user.py
@@ -0,0 +1,14 @@
+# Episode input user
+
+# Data yang dimasukan pasti string
+# data = input("Masukan data: ")
+# print("data = ", data, "type = ", type(data))
+
+# jika ingin mengambil int/float, maka
+# angka = float(input("Masukan data: "))
+# angka = int(input("Masukan data: "))
+# print("data = ", angka, "type = ", type(angka))
+
+# casting input tipe boolean
+biner = bool(int(input("Masukan nilai boolean: ")))
+print("data = ", biner, "type = ", type(biner))
